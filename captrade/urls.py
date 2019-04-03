11008caa6a696d5c1bae4436db59c80b4792990c
Michael Oh <mike.js.oh@gmail.com>
2019-04-02 21:02:29 -0400

    Added Details view for Finance class - Removed unused imports in models.py - Added finance_detail() in views.py - Updated finance_list() in views.py - Added url path for finance details - Added custom.css file - Created base.html and finance_detail.html template - Updated finance_list.html to work with base.html template

--- captrade/urls.py
+++ captrade/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.finance_list, name="finance_list"),
+    path('finance/<int:pk>', views.finance_detail, name="finance_detail")
 ]
--- captrade/views.py
+++ captrade/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Finance
 
 # Create your views here.
 def finance_list(request):
-    return render(request, 'captrade/finance_list.html',{})
+    finances = Finance.objects.order_by('finance_id')
+    return render(request, 'captrade/finance_list.html',{
+        'finances': finances
+    })
+
+def finance_detail(request, pk):
+    finance=get_object_or_404(Finance, pk=pk)
+    return render(request, 'captrade/finance_detail.html', {
+        'finance': finance
+    })
